e16f70b30162801ad9da1476efea2bb5c69056b3
nueton <61454382+nueton@users.noreply.github.com>
2022-07-26 20:15:25 +0700

    uploaded app.js files
    This file is  for the buttons to work.

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+let numberBox = document.querySelector("#numberBox");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const reset = document.querySelector("#reset");
+
+let my_number=0;
+
+plus.addEventListener('click',function(){
+    let existing_number = numberBox.value;
+    my_number = existing_number;
+    my_number++;
+    numberBox.value = my_number;
+    
+})
+
+minus.addEventListener('click',()=>{
+    let existing_number = numberBox.value;
+    my_number = existing_number;
+    my_number--;
+    numberBox.value = my_number;
+})
+
+reset.addEventListener('click',function(){
+    my_number = 0;
+    numberBox.value = my_number;
+})
